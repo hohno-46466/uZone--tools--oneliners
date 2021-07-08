9592bf7b29c70989a636c6bc629caab5591f3a02
hohno-46466 <hohno.46466@gmail.com>
2021-07-08 22:39:22 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2016B.local

---
+++ gamma-correction.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+# Generating gamma correction table in C language format.
+
+seq 0 1 255 | awk ' BEGIN { printf "const uint8_t PROGMEM data[] = {\n"; } { if (NR>1) { printf ",\t" } printf " %d", 255*exp(2.2*log((NR-1)/255.0))+0,5 } END { printf "\n};\n" }' | expand | fold -40
+
+# Written by @hohno_at_kuimc and placed in the public domain
+# Last update: Thu Jul  8 22:35:23 JST 2021
+
+# The one-liner above is equivalent to the following shell script
+
+# seq 0 1 255 |
+# awk '
+#   BEGIN {
+#     printf "const uint8_t PROGMEM data[] = {\n";
+#   }
+#   {
+#     if (NR>1) { printf ",\t" }
+#     printf " %d", 255*exp(2.2*log((NR-1)/255.0))+0,5
+#   }
+#   END {
+#     printf "\n};\n"
+#   }' |
+# expand |
+# fold -40
