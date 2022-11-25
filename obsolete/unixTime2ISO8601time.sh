37e61c74596dbb0d496e8337ac1636a482485770
hohno-46466 <hohno.46466@gmail.com>
2022-11-26 05:19:28 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2022P.local

--- obsolete/unixTime2ISO8601time.sh
+++ obsolete/unixTime2ISO8601time.sh
@@ -2,7 +2,7 @@
 
 # unixTime2ISO8601time.sh
 
-# Prev update: Thu Apr  9 06:34:13 JST 2020 by @hohno_at_kuimc
+# Prev updated: Thu Apr  9 06:34:13 JST 2020 by @hohno_at_kuimc
 # Last updated: Thu Jul 15 22:50:15 JST 2021 by @hohno_at_kuikmc
 
 # Public domain
--- obsolete/unixTime2humanReadableTime.sh
+++ obsolete/unixTime2humanReadableTime.sh
@@ -2,7 +2,7 @@
 
 # unixTime2humanReadableTime.sh
 
-# Prev update: Thu Apr  9 06:34:13 JST 2020 by @hohno_at_kuimc
+# Prev updated: Thu Apr  9 06:34:13 JST 2020 by @hohno_at_kuimc
 # Last updated: Thu Jul 15 22:50:47 JST 2021 by @hohno_at_kuimc
 
 # Public domain
